5b7acbfc1668a23c7956c160a1f11263896c3442
Aman Raj <138397076+amanrajcodes@users.noreply.github.com>
2023-07-10 13:01:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.get("/",(req,res)=>{
-  res.send("helloooo")
+  res.send("Backend working")
 })
 
 // Routes 
